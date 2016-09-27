b2ca2f16c36d6ded3159620028caec833c3fb482
Fred <fred@opendataquality.com>
2016-09-27 16:46:11 +0200

    Login/Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,13 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Invalid email/password combination'
     end
   end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
 end
+
+
+
