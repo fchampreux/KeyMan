1d1ac068ddafdde16a203229a8a8401eb7cb613a
Fred <fred@opendataquality.com>
2017-06-30 11:59:13 +0200

    Parameters WIP

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -69,6 +69,6 @@ class ParametersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def parameter_params
-      params.require(:parameter).permit(:name, :, :description, :, :code, :, :value, :)
+      params.require(:parameter).permit(:name, :description, :code, :value)
     end
 end
