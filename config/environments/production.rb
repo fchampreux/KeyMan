e12ec083d4ab8617a38759863dbf5ec5acd995c9
Fred <fred@nohoo.biz>
2017-07-26 06:20:10 +0200

    email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+      
+  # Mailer configuration
+  config.action_mailer.default_url_options = { :host => 'keyman.nohoo.biz' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
