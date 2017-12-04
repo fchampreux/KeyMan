78375dcc8ab5a87c9661e5e183e02fab1924d16c
Frédéric CHAMPREUX <fchampreux@gmail.com>
2017-12-04 15:08:34 +0100

    Update smtp.rb

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -4,6 +4,6 @@ ActionMailer::Base.smtp_settings = {
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "#############",
-    password: "#############"
+    user_name: "fchampreux@gmail.com",
+    password: "#Emlxx*03!"
     }
