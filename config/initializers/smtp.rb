dcfc93b914734526ec7211fffddef66bbf6b313d
Fred <fred@nohoo.biz>
2017-12-04 17:21:51 +0100

    Remove configuration file from repository

--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "fchampreux@gmail.com",
-    password: "#pass*03!"
-    }
