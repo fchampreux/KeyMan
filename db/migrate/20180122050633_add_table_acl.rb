43a9ffd073491e7d537c1a344e8e92e794678929
Fred <fred@nohoo.biz>
2018-01-22 06:25:06 +0100

    new objects

---
+++ db/migrate/20180122050633_add_table_acl.rb
@@ -0,0 +1,32 @@
+class AddTableAcl < ActiveRecord::Migration[5.0]
+  def change
+    create_table "acl", force: :cascade do |t|
+      t.integer  "key_id",                   null: false
+      t.integer  "user_id",                  null: false
+      t.boolean  "allow_encrypt", default: true
+      t.boolean  "allow_decrypt", default: false
+      t.date     "valid_from",               null: false
+      t.date     "valid_until",              null: false
+      t.datetime "created_at",               null: false
+      t.datetime "updated_at",               null: false
+      t.string   "created_by",   limit: 100
+      t.string   "updated_by",   limit: 100
+    end
+    
+    create_table "audit", force: :cascade do |t|
+      t.integer  "user_id",                  null: false
+      t.integer  "object_id"
+      t.string   "object_class",  limit: 100
+      t.string   "object_name",   limit: 100
+      t.string   "server_name",   limit: 100
+      t.string   "table_name",    limit: 100
+      t.string   "column_name",   limit: 100
+      t.string   "description",   limit: 1000
+      t.boolean  "used_encrypt", default: false
+      t.boolean  "used_decrypt", default: false
+      t.datetime "created_at",               null: false
+      t.string   "created_by",   limit: 100
+    end    
+        
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120154537) do
+ActiveRecord::Schema.define(version: 20180122050633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "acl", force: :cascade do |t|
+    t.integer  "key_id",                                    null: false
+    t.integer  "user_id",                                   null: false
+    t.boolean  "allow_encrypt",             default: true
+    t.boolean  "allow_decrypt",             default: false
+    t.date     "valid_from",                                null: false
+    t.date     "valid_until",                               null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.string   "created_by",    limit: 100
+    t.string   "updated_by",    limit: 100
+  end
+
+  create_table "audit", force: :cascade do |t|
+    t.integer  "user_id",                                   null: false
+    t.integer  "object_id"
+    t.string   "object_class", limit: 100
+    t.string   "object_name",  limit: 100
+    t.string   "server_name",  limit: 100
+    t.string   "table_name",   limit: 100
+    t.string   "column_name",  limit: 100
+    t.string   "description",  limit: 1000
+    t.boolean  "used_encrypt",              default: false
+    t.boolean  "used_decrypt",              default: false
+    t.datetime "created_at",                                null: false
+    t.string   "created_by",   limit: 100
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "name",        limit: 100,                null: false
