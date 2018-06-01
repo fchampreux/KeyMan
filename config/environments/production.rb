6606d271380e7d38c48ae99a807c6c50f31737b3
Fred <fred@opendataquality.com>
2018-06-01 16:07:47 +0200

    Activated https in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -89,7 +89,7 @@ Rails.application.configure do
       
   # Mailer configuration
   config.action_mailer.default_url_options = { :host => 'keyman.nohoo.biz' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :mailgun
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
